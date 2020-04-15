3bb9d442ad840d68c54b43e3d3f2f2785ee5ab2f
allan-viveiros <viveirosallan@gmail.com>
2020-04-14 21:32:41 -0400

    Updating menu items and rotate toggle button

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+// Get DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBrand = document.querySelector('.menu-brand');
+const menuNavItems = document.querySelectorAll('.nav-item');
+
+//Initial state of main menu (Vanilla-js)
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBrand.classList.add('show');
+
+        menuNavItems.forEach(item => item.classList.add('show'));
+
+        //Menu is open
+        showMenu = true;
+    }
+    else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBrand.classList.remove('show');
+
+        menuNavItems.forEach(item => item.classList.remove('show'));
+
+        //Menu is close
+        showMenu = false;
+    }
+}
